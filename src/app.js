5ea77fb8346b8c5882b8660fd239280056ea4215
Dongwoon Son <dplusic@gmail.com>
2018-02-07 00:39:51 +0900

    Add a simple function

--- src/app.js
+++ src/app.js
@@ -4,3 +4,10 @@ const { app } = global;
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+// A simple function
+// https://docs.back4app.com/docs/integrations/command-line-interface/a-simple-function/
+const { Parse } = global;
+Parse.Cloud.define('hello', (req, res) => {
+  res.success('Hello world!');
+});
