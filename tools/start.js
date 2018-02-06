e63dae4f9900caff8f07d70e3a67b281596991f6
Dongwoon Son <dplusic@gmail.com>
2018-02-07 02:17:51 +0900

    Start using parser-server

--- tools/start.js
+++ tools/start.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import { ParseServer } from 'parse-server';
+
+const port = process.env.PORT || 3000;
+
+const parseServerDatabaseURI = process.env.PARSE_SERVER_DATABASE_URI;
+if (!parseServerDatabaseURI) {
+  process.stderr.write('PARSE_SERVER_DATABASE_URI is not set.\n');
+  process.exit(1);
+}
 
 const app = express();
+
+const parseServer = new ParseServer({
+  serverURL: `http://localhost:${port}`,
+  databaseURI: parseServerDatabaseURI,
+  appId: 'myAppId',
+  masterKey: 'myMasterKey',
+});
 
 global.app = app;
 
 require('../src/app');
 
-app.listen(3000);
+app.use('/', parseServer);
+
+app.listen(port);
