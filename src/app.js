136d221f37bad399119fcc588f9df2eab7d0d07c
Dongwoon Son <dplusic@gmail.com>
2018-02-08 00:01:34 +0900

    Connect to database

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
+import ParseServerConfig from 'parse-server/lib/Config';
+
+const { Parse } = global;
+
+const { database } = ParseServerConfig.get(
+  Parse.CoreManager.get('APPLICATION_ID'),
+);
+
 // express app is prepared by Back4App
 const { app } = global;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/', async (req, res) => {
+  const results = await database.find(
+    '_GlobalConfig',
+    { objectId: '1' },
+    { limit: 1 },
+  );
+  res.send(JSON.stringify(results));
 });
 
 // A simple function
 // https://docs.back4app.com/docs/integrations/command-line-interface/a-simple-function/
-const { Parse } = global;
 Parse.Cloud.define('hello', (req, res) => {
   res.success('Hello world!');
 });
