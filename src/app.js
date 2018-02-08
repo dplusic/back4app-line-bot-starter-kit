538faa44420c5d75557983b52078ff9b0f1a58a5
Dongwoon Son <dplusic@gmail.com>
2018-02-09 06:00:54 +0900

    Add sample code
    From https://github.com/line/line-bot-sdk-nodejs/blob/master/examples/echo-bot/index.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,60 @@
+import console from 'console';
 import ParseServerConfig from 'parse-server/lib/Config';
+import {
+  Client as LineBotClient,
+  middleware as lineBotMiddleware,
+} from '@line/bot-sdk';
+import createHandler from './handler';
 
-const { Parse } = global;
+let lineBotMiddlewareFunc;
+let handleEvent;
 
-const { database } = ParseServerConfig.get(
-  Parse.CoreManager.get('APPLICATION_ID'),
-);
+const initPromise = (async () => {
+  try {
+    const { Parse } = global;
+    const { database } = ParseServerConfig.get(
+      Parse.CoreManager.get('APPLICATION_ID'),
+    );
+    const globalConfigs = await database.find(
+      '_GlobalConfig',
+      { objectId: '1' },
+      { limit: 1 },
+    );
+    const env = globalConfigs[0].params;
+
+    // create LINE SDK config from env variables
+    const config = {
+      channelAccessToken: env.LINE_BOT_CHANNEL_ACCESS_TOKEN,
+      channelSecret: env.LINE_BOT_CHANNEL_SECRET,
+    };
+
+    // create LINE SDK client
+    const client = new LineBotClient(config);
+
+    lineBotMiddlewareFunc = lineBotMiddleware(config);
+    handleEvent = createHandler(client);
+  } catch (e) {
+    console.error(e);
+  }
+})();
 
 // express app is prepared by Back4App
 const { app } = global;
 
-app.get('/', async (req, res) => {
-  const results = await database.find(
-    '_GlobalConfig',
-    { objectId: '1' },
-    { limit: 1 },
-  );
-  res.send(JSON.stringify(results));
-});
-
-// A simple function
-// https://docs.back4app.com/docs/integrations/command-line-interface/a-simple-function/
-Parse.Cloud.define('hello', (req, res) => {
-  res.success('Hello world!');
-});
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post(
+  '/callback',
+  async (req, res, next) => {
+    await initPromise;
+    lineBotMiddlewareFunc(req, res, next);
+  },
+  (req, res) => {
+    Promise.all(req.body.events.map(handleEvent))
+      .then(result => res.json(result))
+      .catch(err => {
+        console.error(err);
+        res.status(500).end();
+      });
+  },
+);
---
+++ src/handler.js
@@ -0,0 +1,14 @@
+export default client =>
+  // event handler
+  function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      // ignore non-text-message event
+      return Promise.resolve(null);
+    }
+
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
+
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+  };
